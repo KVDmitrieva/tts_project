0a1c49c081c62bd8584cd87fc02770261fe70501
kvdmitrieva <katedv02@gmail.com>
2023-11-21 23:39:24 +0300

    create alignments from sem

--- hw_tts/model/fast_speech.py
+++ hw_tts/model/fast_speech.py
@@ -193,9 +193,11 @@ class LengthRegulator(nn.Module):
     def _create_alignment(base_mat, duration_predictor_output):
         N, L = duration_predictor_output.shape
         for i in range(N):
-            base_mat[i] = torch.repeat_interleave(
-                torch.eye(L), duration_predictor_output[i], dim=0
-            )
+            count = 0
+            for j in range(L):
+                for k in range(duration_predictor_output[i][j]):
+                    base_mat[i][count + k][j] = 1
+                count = count + duration_predictor_output[i][j]
         return base_mat
 
     def _regulate_length(self, x, duration_predictor_output, mel_max_length=None):
