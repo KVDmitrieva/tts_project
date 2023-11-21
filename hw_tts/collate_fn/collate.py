4708c64b45feec5c6d039d3128b7fdd4579611ad
kvdmitrieva <katedv02@gmail.com>
2023-11-21 21:44:48 +0300

    fix collate

--- hw_tts/collate_fn/collate.py
+++ hw_tts/collate_fn/collate.py
@@ -17,22 +17,20 @@ def collate_fn(dataset_items: List[dict]):
     mel_max_len = 0
 
     for item in dataset_items:
-        if "mel" in item:
-            alignment.append(item["alignment"])
-            mel.append(item["mel"].unsqueeze(0).transpose(1, 2))
-            mel_max_len = max(mel_max_len, item["mel"].shape[-1])
-            mel_pos.append(torch.arange(1, item["mel"].shape[-1] + 1))
-
         text.append(item["text"])
+        mel.append(item["mel"].T)
+        alignment.append(item["alignment"])
         text_encoded.append(item["text_encoded"].squeeze(0))
+        mel_max_len = max(mel_max_len, item["mel"].shape[-1])
+        mel_pos.append(torch.arange(1, item["mel"].shape[-1] + 1))
         src_pos.append(torch.arange(1, text_encoded_length[-1] + 1))
 
     return {
         "text": text,
-        "alignment": alignment if len(mel) > 0 else None,
+        "alignment": alignment,
+        "mel_max_len": mel_max_len,
+        "mel_pos": pad_sequence(mel_pos, batch_first=True),
         "src_pos": pad_sequence(src_pos, batch_first=True),
-        "mel_max_len": alignment if len(mel) > 0 else None,
         "text_encoded": pad_sequence(text_encoded, batch_first=True),
-        "mel_pos": pad_sequence(mel_pos, batch_first=True) if len(mel) > 0 else None,
-        "mel_target":  pad_sequence(mel, batch_first=True).transpose(1, 2) if len(mel) > 0 else None
+        "mel_target":  pad_sequence(mel, batch_first=True).transpose(1, 2)
     }
