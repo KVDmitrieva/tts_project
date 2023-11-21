ba8c134b0e956bc8eb4ab4bee0a6cc953a5a65d9
kvdmitrieva <katedv02@gmail.com>
2023-11-21 19:53:59 +0300

    fix dataset

--- hw_tts/datasets/ljspeech_dataset.py
+++ hw_tts/datasets/ljspeech_dataset.py
@@ -22,8 +22,9 @@ class LJspeechDataset(BaseDataset):
     def __init__(self, part, mel_dir=None, alignments_dir=None, data_dir=None,
                  text_path=None, train_ratio=0.92, *args, **kwargs):
         if data_dir is None:
-            data_dir = ROOT_PATH / "data" / "datasets" / "ljspeech"
+            data_dir = ROOT_PATH / "data" / "datasets"
             data_dir.mkdir(exist_ok=True, parents=True)
+            data_dir /= "ljspeech"
         self._data_dir = data_dir
         self._train_ratio = train_ratio
         self._mel_dir = ROOT_PATH / "data" / "mels" if mel_dir is None else Path(mel_dir)
@@ -35,6 +36,7 @@ class LJspeechDataset(BaseDataset):
         super().__init__(index, *args, **kwargs)
 
     def _load_dataset(self):
+        self._data_dir.mkdir(exist_ok=True, parents=True)
         arch_path = self._data_dir / "LJSpeech-1.1.tar.bz2"
         print(f"Loading LJSpeech")
         download_file(URL_LINKS["dataset"], arch_path)
@@ -67,9 +69,9 @@ class LJspeechDataset(BaseDataset):
         end_ind = int(self._train_ratio * len(text)) if part == "train" else len(text)
 
         for w_id in tqdm(range(start_ind, end_ind), desc=f"Processing {part} data"):
-            mel_path = self._mel_dir / "ljspeech-mel-%05d.npy" % w_id
+            mel_path = self._mel_dir / ("ljspeech-mel-%05d.npy" % w_id)
             alignment_path = self._alignment_dir / f"{w_id}.npy"
-            wav_path = self._data_dir / "%05d.wav" % w_id
+            wav_path = self._data_dir / ("%05d.wav" % w_id)
             t_info = torchaudio.info(str(wav_path))
             length = t_info.num_frames / t_info.sample_rate
             index.append(
