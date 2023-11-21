dbb1046d7664564b35a323d6c73342b815fa5b4b
kvdmitrieva <katedv02@gmail.com>
2023-11-22 02:02:07 +0300

    debug loss

--- hw_tts/loss/FastSpeechLoss.py
+++ hw_tts/loss/FastSpeechLoss.py
@@ -8,6 +8,10 @@ class FastSpeechLoss(nn.Module):
         self.l1_loss = nn.L1Loss()
 
     def forward(self, mel, duration_predicted, mel_target, alignment, **batch):
+        print("MEL", mel.shape, mel)
+        print("MEL TARGET", mel_target.shape, mel_target)
+        print("DUR PRED", duration_predicted.shape, duration_predicted)
+        print("AL", alignment.shape, alignment)
         mel_loss = self.mse_loss(mel, mel_target)
         duration_predictor_loss = self.l1_loss(duration_predicted, alignment.float())
         return mel_loss, duration_predictor_loss
