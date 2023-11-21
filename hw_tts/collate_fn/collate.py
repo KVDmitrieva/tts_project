3e25d8b8c397b21caed0749666940068272ea301
kvdmitrieva <katedv02@gmail.com>
2023-11-21 14:28:40 +0300

    remove metrics & change collate fn

--- hw_tts/collate_fn/collate.py
+++ hw_tts/collate_fn/collate.py
@@ -1,6 +1,7 @@
 import logging
 import torch
 from typing import List
+from torch.nn.utils.rnn import pad_sequence
 
 logger = logging.getLogger(__name__)
 
@@ -9,24 +10,34 @@ def collate_fn(dataset_items: List[dict]):
     """
     Collate and pad fields in dataset items
     """
-    spectrogram, text_encoded = [], []
+    mel, text_encoded = [], []
     text, text_encoded_length = [], []
-    spectrogram_length, audio_path = [], []
+    mel_length, audio_path = [], []
+    alignment, audio = [], []
+    src_pos, mel_pos = [], []
+    mel_max_len = 0
 
     for item in dataset_items:
+        if "mel" in item:
+            alignment.append(item["alignment"])
+            mel.append(item["mel"].unsqueeze(0).transpose(1, 2))
+            mel_max_len = max(mel_max_len, item["mel"].shape[-1])
+            mel_pos.append(torch.arange(1, item["mel"].shape[-1] + 1))
+
         text.append(item["text"])
+        audio.append(item["audio"])
         audio_path.append(item["audio_path"])
-        spectrogram.append(item["spectrogram"].squeeze(0).T)
         text_encoded.append(item["text_encoded"].squeeze(0))
-        spectrogram_length.append(item["spectrogram"].shape[2])
-        text_encoded_length.append(item["text_encoded"].shape[1])
+        src_pos.append(torch.arange(1, text_encoded_length[-1] + 1))
 
     return {
         "text": text,
-        "audio": item["audio"],
+        "audio": audio,
         "audio_path": audio_path,
-        "spectrogram_length": torch.tensor(spectrogram_length),
-        "text_encoded_length": torch.tensor(text_encoded_length),
-        "text_encoded": torch.nn.utils.rnn.pad_sequence(text_encoded, batch_first=True),
-        "spectrogram":  torch.nn.utils.rnn.pad_sequence(spectrogram, batch_first=True).transpose(1, 2)
+        "alignment": alignment if len(mel) > 0 else None,
+        "src_pos": pad_sequence(src_pos, batch_first=True),
+        "mel_max_len": alignment if len(mel) > 0 else None,
+        "text_encoded": pad_sequence(text_encoded, batch_first=True),
+        "mel_pos": pad_sequence(mel_pos, batch_first=True) if len(mel) > 0 else None,
+        "mel_target":  pad_sequence(mel, batch_first=True).transpose(1, 2) if len(mel) > 0 else None
     }
--- hw_tts/metric/__init__.py
+++ hw_tts/metric/__init__.py
@@ -1,11 +1,3 @@
-from hw_tts.metric.cer_metric import ArgmaxCERMetric, BeamSearchCERMetric, LMBeamSearchCERMetric
-from hw_tts.metric.wer_metric import ArgmaxWERMetric, BeamSearchWERMetric, LMBeamSearchWERMetric
 
 __all__ = [
-    "ArgmaxWERMetric",
-    "ArgmaxCERMetric",
-    "BeamSearchWERMetric",
-    "BeamSearchCERMetric",
-    "LMBeamSearchWERMetric",
-    "LMBeamSearchCERMetric",
 ]
