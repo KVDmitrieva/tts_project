85fa520a556d7858b06b8999500d235ec30623de
kvdmitrieva <katedv02@gmail.com>
2023-11-22 19:41:23 +0300

    variance adaptor

--- hw_tts/model/fastspeech2.py
+++ hw_tts/model/fastspeech2.py
@@ -48,29 +48,47 @@ class UniversalPredictor(nn.Module):
 
 
 class VarianceAdaptor(nn.Module):
-    def __init__(self, predictor_params):
+    def __init__(self, predictor_params, n_bins=256):
         super().__init__()
         self.length_regulator = LengthRegulator(**predictor_params, predictor=UniversalPredictor)
         self.pitch_predictor = UniversalPredictor(**predictor_params)
         self.energy_predictor = UniversalPredictor(**predictor_params)
 
-    def forward(self, x, alpha=1.0, target=None, mel_max_length=None):
-        output, duration_predictor_output = self.length_regulator(x, alpha, target, mel_max_length)
-        pitch_out = self.pitch_predictor(output)
-        energy_out = self.energy_predictor(output)
+        self.n_bins = 256
+        self.pitch_embedding = nn.Embedding(n_bins, predictor_params["encoder_dim"])
+        self.energy_embedding = nn.Embedding(n_bins, predictor_params["encoder_dim"])
 
+    def _extract_pitch(self, x, pitch_alpha=1.0, pitch=None):
+        pitch_prediction = self.pitch_predictor(x)
+        output = pitch_alpha * pitch_prediction if pitch is None else pitch
+        log_out = torch.log(output + 1e-12)
+        boundaries = torch.exp(torch.linspace(log_out.min(), log_out.max(), self.n_bins - 1))
+        output = self.pitch_embedding(torch.bucketize(output, boundaries))
+        return output, pitch_prediction
+
+    def _extract_energy(self, x, energy_alpha=1.0, energy=None):
+        energy_prediction = self.energy_predictor(x)
+        output = energy_alpha * energy_prediction if energy is None else energy
+        boundaries = torch.linspace(output.min(), output.max(), self.n_bins - 1)
+        output = self.energy_embedding(torch.bucketize(output, boundaries))
+        return output, energy_prediction
+
+    def forward(self, x, alpha=1.0, pitch_alpha=1.0, energy_alpha=1.0,
+                alignment=None, mel_max_length=None, pitch=None, energy=None, **batch):
+        output, duration_predictor_output = self.length_regulator(x, alpha, alignment, mel_max_length)
+        pitch_out, pitch_prediction = self._extract_pitch(output, pitch_alpha=pitch_alpha, pitch=pitch)
+        energy_out, energy_prediction = self._extract_energy(output, energy_alpha=energy_alpha, energy=energy)
         output += pitch_out + energy_out
-
-        return output
+        return output, duration_predictor_output, pitch_prediction, energy_prediction
 
 
 class FastSpeech2(BaseModel):
     """ FastSpeech 2"""
-    def __init__(self, encoder_params, lr_params, decoder_params, num_mels):
+    def __init__(self, encoder_params, va_params, decoder_params, num_mels):
         super().__init__()
 
         self.encoder = Encoder(**encoder_params)
-        self.length_regulator = LengthRegulator(**lr_params, predictor=UniversalPredictor)
+        self.variance = VarianceAdaptor(**va_params)
         self.decoder = Decoder(**decoder_params)
 
         self.mel_linear = nn.Linear(decoder_params["decoder_dim"], num_mels)
@@ -82,18 +100,19 @@ class FastSpeech2(BaseModel):
         mask = mask.unsqueeze(-1).expand(-1, -1, mel_output.size(-1))
         return mel_output.masked_fill(mask, 0.)
 
-    def forward(self, text_encoded, src_pos, mel_pos=None, mel_max_length=None, alignment=None, alpha=1.0, **batch):
+    def forward(self, text_encoded, src_pos, mel_pos=None, mel_max_length=None, alpha=1.0, pitch_alpha=1.0, energy_alpha=1.0, **batch):
         x, _ = self.encoder(text_encoded, src_pos)
-        output, duration_predictor_output = self.length_regulator(x, alpha, alignment, mel_max_length)
+        output, duration_out, pitch_out, energy_out = self.variance(x, alpha, mel_max_length=mel_max_length, **batch)
         output = self.decoder(output, mel_pos)
         output = self._mask_tensor(output, mel_pos, mel_max_length)
         output = self.mel_linear(output)
-        return {"mel": output, "duration_predicted": duration_predictor_output}
+        return {"mel": output, "duration_predicted": duration_out,
+                "pitch_predicted": pitch_out, "energy_predicted": energy_out}
 
     @torch.inference_mode
-    def inference(self, text_encoded, src_pos, alpha=1.0, **batch):
+    def inference(self, text_encoded, src_pos, alpha=1.0, pitch_alpha=1.0, energy_alpha=1.0, **batch):
         x, _ = self.encoder(text_encoded, src_pos)
-        output, mel_pos = self.length_regulator(x, alpha)
+        output, mel_pos, _, _ = self.variance(x, alpha, pitch_alpha, energy_alpha)
         output = self.decoder(output, mel_pos)
         output = self.mel_linear(output)
         return {"mel": output}
