81e83172b0228ce844cb0117ccabcd4060b084ff
kvdmitrieva <katedv02@gmail.com>
2023-11-23 14:09:47 +0300

    debug

--- hw_tts/loss/FastSpeechV2Loss.py
+++ hw_tts/loss/FastSpeechV2Loss.py
@@ -8,11 +8,11 @@ class FastSpeechV2Loss(nn.Module):
         self.l1_loss = nn.L1Loss()
 
     def forward(self, mel, duration_predicted, pitch_predicted, energy_predicted,
-                mel_target, alignment, pitch_t, energy_t, **batch):
+                mel_target, alignment, pitch, energy, **batch):
         mel_loss = self.mse_loss(mel, mel_target)
         duration_predictor_loss = self.l1_loss(duration_predicted, alignment.float())
-        pitch_loss = self.mse_loss(pitch_predicted, pitch_t)
-        energy_loss = self.mse_loss(energy_predicted, energy_t)
+        pitch_loss = self.mse_loss(pitch_predicted, pitch.float())
+        energy_loss = self.mse_loss(energy_predicted, energy.float())
         return {
             "loss": mel_loss + duration_predictor_loss + pitch_loss + energy_loss,
             "mel_loss": mel_loss,
